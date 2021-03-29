b8356e74b51f518e04cdd01e2ba49cd545b2c04a
Georgia Hunt <georgialhunt89@gmail.com>
2021-03-29 12:41:51 +1030

    Added questions to new questions.js file & HTML

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,50 +2,6 @@
 var secondsLeft = 45;
 var timeEl = document.querySelector('#countdown-timer');
 var buttonEl = document.querySelector('.button')
-// Questions, answers & correct answers
-const questions = [
-    { // First question
-        questionOne: 'Commonly used data types DO NOT include:',
-        answers: {
-            a: 'Strings',
-            b: 'Booleans',
-            c: 'Alerts',
-            d: 'Numbers',    
-        },
-        correctAnswer: 'c'
-    },
-    { // Second question
-        questionTwo: 'The condition in an if / else statment is enclosed within _______:',
-        answers: {
-            a: 'Quotes',
-            b: 'Curly Brackets',
-            c: 'Parenthesis',
-            d: 'Square Brackets',    
-        },
-        correctAnswer: 'c'
-    },
-    { // Third question
-        questionThree: 'Arrays in JavaScript can be used to store:',
-        answers: {
-            a: 'Numbers and strings',
-            b: 'Booleans',
-            c: 'Other arrays',
-            d: 'All of the above',    
-        },
-        correctAnswer: 'd'
-    },
-    { // Fourth question
-        questionFour: 'String values must be enclosed within______ when being assigned to values:',
-        answers: {
-            a: 'Quotes',
-            b: 'Curly Brackets',
-            c: 'Parenthesis',
-            d: 'Square Brackets',    
-        },
-        correctAnswer: 'a'
-    },
-]
-
 
 // Start quiz timer
 function startQuiz() {
