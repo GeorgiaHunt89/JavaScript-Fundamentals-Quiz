6cc2c0bb929dd9a72036575a6ab48222c14d1a0d
Georgia Hunt <georgialhunt89@gmail.com>
2021-03-29 13:34:21 +1030

    Quiz Section added to HTML

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -4,7 +4,7 @@ var timeEl = document.querySelector('#countdown-timer');
 var buttonEl = document.querySelector('.button')
 
 // Start quiz timer
-function startQuiz() {
+function startTimer() {
     var timerInterval = setInterval(function(){
         secondsLeft = secondsLeft - .1;
         timeEl.textContent = secondsLeft.toFixed(2) + ' seconds left';
@@ -16,8 +16,17 @@ function startQuiz() {
     }, 100);
 }
 
+function sendMessage() {
+    timerEL.textContent = 'Time is up, better luck next time!';
+}
+
+// Function to start quiz with Start button
 buttonEl.addEventListener('click', function(){
-    startQuiz()
+    startTimer()
 })
+
+function startQuiz(){
+    
+}
 
 
